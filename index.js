fc725dedd9bc75af7a4d4281276c9ce3220a5bf7
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-08 01:30:08 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+  function send(message) {
+
+    let params = {
+      active: true,
+      currentWindow: true
+    }
+    chrome.tabs.query(params, gotTabs);
+
+    function gotTabs(tabs) {
+      console.log("got tabs");
+      console.log(tabs);
+      // send a message to the content script
+     // let message = userinput.value();
+      let msg = {
+        message: message
+      };
+      chrome.tabs.sendMessage(tabs[0].id, msg);
+    }
+
+  }
+  	send("Scan!");
+  
+  setTimeout(function(){window.close() }, 5000);
